0be58c7b34e2784415a482b08dd5dba7256ced37
jfl99 <47549207+jfl99@users.noreply.github.com>
2024-03-06 09:54:20 +0800

    Update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+  
+   // Add basePath
+    basePath: '/open-react-template',
+  
+  }
+  
+  module.exports = nextConfig
